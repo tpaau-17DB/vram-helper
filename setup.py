4130c9407660a8f0f726c865de69735de9fef800
0x4248 <60709927+0x4248@users.noreply.github.com>
2024-06-19 21:51:50 +0100

    Make python package
    Signed-off-by: 0x4248 <60709927+0x4248@users.noreply.github.com>

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    name='vram_helper',
+    version='1.0',
+    packages=['vram_helper'],
+    install_requires=[]
+)
---
+++ vram_helper/__init__.py
@@ -0,0 +1,6 @@
+"""
+Simple python script that sends warnings about VRAM usage.
+Uses notify-send by default
+
+This file is left empty intentionally.
+"""
